36df8076a8e83fa92a3ee091125dd6a46df0e6b1
Saantiagoandres <santiagovic2508@gmail.com>
2024-04-09 21:45:12 -0500

    Completo

--- lib/screens/Port.dart
+++ lib/screens/Port.dart
@@ -21,35 +21,59 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 100,
-        title: const Text("Hidden Talents"),
-        centerTitle: true,
-        backgroundColor: Color.fromARGB(255, 138, 80, 231),
-        actions: [
-          Builder(
-            builder: (BuildContext context) {
-              return IconButton(
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-                icon: Icon(Icons.menu),
-              );
-            },
-          ),
-        ],
-        leading: Image.asset(
-          "assets/images/logo.png",
-          height: 100.0,
-        ),
-      ),
+appBar: AppBar(
+  toolbarHeight: 120, // Ajusta la altura del AppBar según el tamaño del logo
+  title: const Text("Hidden Talents"),
+  centerTitle: true,
+  backgroundColor: Color.fromARGB(255, 158, 117, 224),
+  actions: [
+    IconButton(
+      onPressed: () {
+        // Acción al presionar el botón de búsqueda
+      },
+      icon: Icon(Icons.search),
+    ),
+    IconButton(
+      onPressed: () {
+        // Acción al presionar el botón de notificaciones
+      },
+      icon: Icon(Icons.notifications),
+    ),
+    Builder(
+      builder: (BuildContext context) {
+        return IconButton(
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+          icon: Icon(Icons.menu),
+        );
+      },
+    ),
+    IconButton(
+      onPressed: () {
+        // Acción al presionar el botón de perfil
+      },
+      icon: Icon(Icons.person),
+    ),
+  ],
+  leading: Container(
+    margin: EdgeInsets.all(0.0), // Margen alrededor del logo
+    width: 120, // Ancho del logo
+    height: 100, // Alto del logo
+    child: Image.asset(
+      "assets/images/logo.png",
+      fit: BoxFit.scaleDown, // Ajusta la imagen dentro del contenedor sin distorsionarla
+    ),
+  ),
+),
+
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
             DrawerHeader(
               decoration: BoxDecoration(
-                color: Color.fromARGB(255, 104, 168, 211),
+                color: Color.fromARGB(255, 90, 219, 228),
               ),
               child: Image.asset(
                 "assets/images/logo.png",
@@ -90,48 +114,70 @@ class HomePage extends StatelessWidget {
 class UserList extends StatelessWidget {
   const UserList({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Lista de Usuario de la Api'),
-      ),
-      body: FutureBuilder<List<dynamic>>(
-        future: fetchData(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            if (snapshot.data != null) {
-              return ListView.builder(
-                itemCount: snapshot.data?.length ?? 0,
-                itemBuilder: (context, index) {
-                  final userData = snapshot.data![index];
-                  return ListTile(
-                    title: Text('Nombre: ${userData['name'] ?? ''}'),
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Lista de Usuario de la Api'),
+      centerTitle: true,
+    ),
+    body: FutureBuilder<List<dynamic>>(
+      future: fetchData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          if (snapshot.data != null) {
+            return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) {
+                final userData = snapshot.data![index];
+                return Center( // Centra todo el ListTile
+                  child: ListTile(
+                    title: Text(
+                      'Nombre: ${userData['name'] ?? ''}',
+                      textAlign: TextAlign.center,
+                    ),
                     subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center, // Centra el texto dentro de la columna
                       children: [
-                        Text('Apellido: ${userData['lastname'] ?? ''}'),
-                        Text('Telefono: ${userData['telefono'] ?? ''}'),
-                        Text('Email: ${userData['email'] ?? ''}'),
-                        Text('Password: ${userData['password'] ?? ''}'),
-                        Text('Role: ${userData['role']['nombre_rol'] ?? ''}'),
+                        Text(
+                          'Apellido: ${userData['lastname'] ?? ''}',
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          'Telefono: ${userData['telefono'] ?? ''}',
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          'Email: ${userData['email'] ?? ''}',
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          'Password: ${userData['password'] ?? ''}',
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          'Role: ${userData['role']['nombre_rol'] ?? ''}',
+                          textAlign: TextAlign.center,
+                        ),
                       ],
                     ),
-                  );
-                },
-              );
-            } else {
-              return Center(child: Text('No data available'));
-            }
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(child: Text('No data available'));
           }
-        },
-      ),
-    );
-  }
+        }
+      },
+    ),
+  );
+}
+
 
   Future<List<dynamic>> fetchData() async {
     try {
@@ -152,52 +198,70 @@ class UserList extends StatelessWidget {
 
 class MyFooter extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 250.0, // Define la altura deseada
-      padding: EdgeInsets.all(10.0),
-      color: Color.fromARGB(255, 138, 80, 231),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset(
-                'assets/images/perfil1.png',
-                width: 100, // Ancho deseado de la imagen
-                height: 100, // Alto deseado de la imagen
-              ),
-              _buildFooterIcon(Icons.audiotrack),
-              _buildFooterIcon(Icons.fingerprint),
-              _buildFooterIcon(Icons.call),
-              Image.asset(
-                'assets/images/perfil1.png',
-                width: 180, // Ancho deseado de la imagen
-                height: 180, // Alto deseado de la imagen
-              ),
-            ],
-          ),
-          const Text(
-            'Copyright ©2024, All Rights Reserved.',
-            style: TextStyle(
-              fontWeight: FontWeight.w300,
-              fontSize: 12.0,
-              color: Color(0xFF162A49),
-            ),
-          ),
-          const Text(
-            'Powered by Your Company',
-            style: TextStyle(
-              fontWeight: FontWeight.w300,
-              fontSize: 12.0,
-              color: Color(0xFF162A49),
-            ),
-          ),
-        ],
+Widget build(BuildContext context) {
+  return Container(
+    height: 250.0,
+    padding: EdgeInsets.all(10.0),
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        colors: [Color(0xFF9E47E7), Color.fromARGB(224, 255, 197, 161)],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
       ),
-    );
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              width: 120,
+              height: 120,
+            ),
+            _buildFooterIcon(Icons.camera),
+            _buildFooterIcon(Icons.email),
+            _buildFooterIcon(Icons.facebook),
+            _buildFooterIcon(Icons.location_on),
+            _buildFooterIcon(Icons.phone),
+            Image.asset(
+              'assets/images/logo.png',
+              width: 120,
+              height: 120,
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Text(
+            '¡Conéctate con nosotros en redes sociales!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 220, 16, 16),
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Text(
+            'Hidden Talents - Descubre tu talento oculto',
+            style: TextStyle(
+              color: const Color.fromARGB(255, 240, 10, 10),
+              fontSize: 14.0,
+              fontStyle: FontStyle.italic,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
   }
 
   Widget _buildFooterIcon(IconData iconData) {
@@ -220,4 +284,4 @@ class MyFooter extends StatelessWidget {
       ),
     );
   }
-}
+
